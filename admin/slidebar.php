506f90ce788bf719415d8079730ec8494b2e675f
Cao Huy Hưng <huyhungdp@gmail.com>
2025-03-21 01:33:01 +0700

    dashboard

---
+++ admin/slidebar.php
@@ -0,0 +1,8 @@
+<!-- sidebar.php -->
+<div class="sidebar">
+    <a href="dashboard.php"><i class="fas fa-tachometer-alt"></i></a>
+    <a href="users.php"><i class="fas fa-users"></i></a>
+    <a href="products.php"><i class="fas fa-shoe-prints"></i></a>
+    <a href="invoices.php"><i class="fas fa-file-invoice"></i></a>
+    <a href="../logout.php"><i class="fas fa-sign-out-alt"></i></a>
+</div>
